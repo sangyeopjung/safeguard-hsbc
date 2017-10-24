e5423f030583e1a2cfa5cb41cc9bf1a56b859e70
sangyeopjung <syjung812@gmail.com>
2017-10-24 22:00:27 +0800

    Chat client: update info functions

--- app/src/main/java/hk/ust/cse/safeguardhsbc/ChatClient.java
+++ app/src/main/java/hk/ust/cse/safeguardhsbc/ChatClient.java
@@ -22,7 +22,11 @@ public class ChatClient extends AppCompatActivity implements View.OnClickListene
     ListView messageList;
     MyAdapter myAdapter = null;
     ArrayList<MessageBubble> messageBubbles = null;
-    String URL = "http://10.89.220.20:10007";
+
+    //private final String URL = "http://10.89.220.20:10007";
+    private final String URL = "http://192.168.0.199:10007";
+    private String ID;
+    private State state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +49,12 @@ public class ChatClient extends AppCompatActivity implements View.OnClickListene
 
         // Set the ListView's adapter to be the adapter that we just constructed.
         messageList.setAdapter((ListAdapter) myAdapter);
+
+        // TODO: Should be carried over from login intent
+        ID = "543210";
+        state = State.MAIN;
+        sendMessage("Welcome to HSBC!");
+        sendMessage(state);
     }
 
     @Override
@@ -63,7 +73,12 @@ public class ChatClient extends AppCompatActivity implements View.OnClickListene
                     // Notify the adapter that the data has changed due to the addition
                     // of a new messageBubble object. This triggers an update of the ListView
                     myAdapter.notifyDataSetChanged();
-                    updatePhoneNum(messageText.getText().toString());
+
+                    state = update(state, message);
+                    // Update customer information
+                    sendMessage(state);
+
+                    //Reset
                     messageBubble = null;
                     messageText.setText("");
                 }
@@ -74,7 +89,65 @@ public class ChatClient extends AppCompatActivity implements View.OnClickListene
         }
     }
 
-    public class UpdatePhoneAsyncTask extends AsyncTask<String, Void, String> {
+    private State update(State st, String msg) {
+        switch(st) {
+            case PHONE:
+                updatePhone(msg);
+                break;
+            case ADDRESS:
+                updateAddress(msg);
+                break;
+            case EMAIL:
+                updateEmail(msg);
+                break;
+            default:
+                try {
+                    return State.values()[Integer.parseInt(msg)];
+                } catch (Exception e) {
+                    sendMessage("Invalid operation. Please re-enter.");
+                }
+                break;
+        }
+        return State.MAIN;
+    }
+
+    private void sendMessage(State st) {
+        String msg;
+        switch(st) {
+            case MAIN:
+                msg = "Please select an operation:\n" +
+                        "1. Update phone number\n" +
+                        "2. Update residential address\n" +
+                        "3. Update email address";
+                break;
+            case PHONE:
+                msg = "Please input your new phone number";
+                break;
+            case ADDRESS:
+                msg = "Please input your new residential address";
+                break;
+            case EMAIL:
+                msg = "Please input your new email address";
+                break;
+            default:
+                msg = "Invalid operation. Please re-enter.";
+                break;
+        }
+        sendMessage(msg);
+    }
+
+    private void sendMessage(String msg) {
+        MessageBubble messageBubble = new MessageBubble(msg, false, new Date());
+        messageBubbles.add(messageBubble);
+        myAdapter.notifyDataSetChanged();
+    }
+
+    private class UpdateAsyncTask extends AsyncTask<String, Void, String> {
+        String dataToUpdate;
+        public UpdateAsyncTask(String dataToUpdate) {
+            this.dataToUpdate = dataToUpdate;
+        }
+
         @Override
         protected String doInBackground(String... params) {
             PutUtility putUtility;
@@ -82,7 +155,7 @@ public class ChatClient extends AppCompatActivity implements View.OnClickListene
             try {
                 putUtility = new PutUtility(URL + "/api/account/updateAccount");
                 putUtility.setContentType("application/json");
-                putUtility.setRequestBody("{ \"id\" : \"543210\", \"phoneNumber\" : \"43214321\" }");
+                putUtility.setRequestBody("{ \"id\" : \"" + ID + "\", \"" + dataToUpdate + "\" : \""+ params[0] + "\" }");
                 response = putUtility.getResponse();
                 System.out.println(response);
             } catch (IOException e) {
@@ -97,33 +170,43 @@ public class ChatClient extends AppCompatActivity implements View.OnClickListene
         }
     }
 
-    public void updatePhoneNum(String pNum){
-        String response = "";
-        UpdatePhoneAsyncTask updatePhoneAsyncTask = new UpdatePhoneAsyncTask();
+    public void updatePhone(String phone){
+        String response;
+        UpdateAsyncTask updateAsyncTask = new UpdateAsyncTask("phoneNumber");
         try {
-            response = updatePhoneAsyncTask.execute("asd").get();
+            response = updateAsyncTask.execute(phone).get();
         } catch (Exception e) {
+            response = "Failed to update your phone number";
             e.printStackTrace();
         }
 
-        if(! "".equals(response))
-            sendMessage(response);
-        else
-            sendMessage("Failed to update your phone number");
+        sendMessage(response);
     }
 
     public void updateAddress(String address) {
+        String response;
+        UpdateAsyncTask updateAsyncTask = new UpdateAsyncTask("residentialAddress");
+        try {
+            response = updateAsyncTask.execute(address).get();
+        } catch (Exception e) {
+            response = "Failed to update your residential address";
+            e.printStackTrace();
+        }
 
+        sendMessage(response);
     }
 
     public void updateEmail(String email) {
+        String response;
+        UpdateAsyncTask updateAsyncTask = new UpdateAsyncTask("email");
+        try {
+            response = updateAsyncTask.execute(email).get();
+        } catch (Exception e) {
+            response = "Failed to update your email address";
+            e.printStackTrace();
+        }
 
-    }
-
-    public void sendMessage(String msg) {
-            MessageBubble messageBubble = new MessageBubble(msg, false, new Date());
-            messageBubbles.add(messageBubble);
-            myAdapter.notifyDataSetChanged();
+        sendMessage(response);
     }
 }
 
---
+++ app/src/main/java/hk/ust/cse/safeguardhsbc/State.java
@@ -0,0 +1,12 @@
+package hk.ust.cse.safeguardhsbc;
+
+/**
+ * Created by Sang Yeop Jung on 24/10/2017.
+ */
+
+public enum State {
+    MAIN,
+    PHONE,
+    ADDRESS,
+    EMAIL;
+}
